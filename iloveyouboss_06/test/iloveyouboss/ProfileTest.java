5be96359e9e79fd0ab64d3d633cc574a7e2254c7
Jinkyu Han <hjk1019@gmail.com>
2020-05-08 21:02:42 +0900

    'JUnit 진지하게 보기'에 대한 내용 추가

--- iloveyouboss_06/test/iloveyouboss/ProfileTest.java
+++ iloveyouboss_06/test/iloveyouboss/ProfileTest.java
@@ -10,15 +10,30 @@ package iloveyouboss;
 
 import org.junit.*;
 
+import static org.junit.Assert.assertFalse;
+
 public class ProfileTest {
 
    @Test
-   public void test() {
+   public void matchAnswersFalseWhenMatchCriteriaNotMet() {
+      /* 준비 */
+      // 프로필을 작성한다
       Profile profile = new Profile("Bull Hockey, Inc.");
+
+      // 질문을 작성하고 답변을 작성하여 프로필에 추가한다
       Question question = new BooleanQuestion(1, "Got bonuses?");
-      Criteria criteria = new Criteria();
       Answer criteriaAnswer = new Answer(question, Bool.TRUE);
+      profile.add(criteriaAnswer);
+
+      // 답변에 대한 기준(Criterion)을 작성하고 Criteria 컬렉션에 추가한다
+      Criteria criteria = new Criteria();
       Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
       criteria.add(criterion);
+
+      /* 실행 */
+      boolean matches = profile.matches(criteria);
+
+      /* 단언 */
+      assertFalse(matches);
    }
 }
